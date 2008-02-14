87367b06b3a8fac06092a40c4075c837d147b982
William Arnold <ash@dragonpaw.org>
2008-02-14 20:39:34 +0000

    Overhaul of the mining op module. Now mases all shares on hours worked * a multiplier. (So maybe the Rorq pilot gets 1.5 shares per hour...)

--- mining/models.py
+++ mining/models.py
@@ -7,9 +7,10 @@ class MiningOp(models.Model):
     datetime = models.DateTimeField()
     description = models.CharField(max_length=200, blank=True)
     created_by = models.ForeignKey(User)
+    hours = models.FloatField(editable=False, default=0)
     
     class Admin:
-        pass
+        list_display = ['id', 'description', 'hours']
     
     class Meta:
         pass
@@ -23,28 +24,7 @@ class MiningOp(models.Model):
     @property
     def name(self):
         return self.description
-
-class MiningOpPhase(models.Model):
-    operation = models.ForeignKey(MiningOp, related_name='phases',
-                                  num_extra_on_change = 4)
-    description = models.CharField(max_length=200, blank=True)
-    shares = models.FloatField(editable=False, default=0)
     
-    class Admin:
-        list_display = ['id', 'operation', 'description', 'shares']
-    
-    def __str__(self):
-        return "Mining Op Phase: %s: %s" % (self.datetime, self.longdesc)
-    
-    @property
-    def datetime(self):
-        return self.operation.datetime
-    
-    @property
-    def longdesc(self):
-        return "%s: %s" % (self.operation.description, self.description)
-    
-    @property
     def sharesizelist(self):
         list = [{'type':x.type, 'quantity':float(x.quantity)/self.shares} for x in self.ores.all()]
         return list
@@ -54,42 +34,52 @@ class MiningOpPhase(models.Model):
         return dict([ (x['type'], x['quantity']/self.shares) for x in self.ores.all() ])
     
 class MinerOnOp(models.Model):
-    phase = models.ForeignKey(MiningOpPhase, related_name='miners',
+    op = models.ForeignKey(MiningOp, related_name='miners',
                               min_num_in_admin = 10,
                               num_extra_on_change = 4, 
                               edit_inline = models.TABULAR)
-    user = models.ForeignKey(User, core=True)
-    share = models.FloatField(default=1.0)
+    name = models.CharField(max_length=100, core=True)
+    multiplier = models.FloatField(default=1.0)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
 
     class Admin:
-        list_display = ['id', 'phase', 'user', 'share',]
+        list_display = ['id', 'op', 'name', 'multiplier', 'hours', 'percent']
         ordering = ['phase']
     
+    def hours(self):
+        x = self.end_time - self.start_time
+        return (x.days * 24) + (x.seconds / 60.0**2)
+    
+    def percent(self):
+        total_hours = self.op.hours
+        return (self.hours() * self.multiplier) / total_hours
+    
     def __str__(self):
-        return "%s [%1.1f]: %s" % (self.user, self.share, self.phase.longdesc)
+        return "%s [%1.2f]" % (self.name, self.hours())
     
     def save(self):
         super(MinerOnOp, self).save() # Call the "real" save() method.
 
         # Update the current balance.
         # (We do it like this so edits don't make -very- odd things happen.
-        shares = 0
-        for row in self.phase.miners.all():
-            shares += row.share
-        self.phase.shares = shares
-        self.phase.save()
+        hours = 0
+        for row in self.op.miners.all():
+            hours += row.hours() * row.multiplier
+        self.op.hours = hours
+        self.op.save()
     
-class MiningOpOre(models.Model):
-    phase = models.ForeignKey(MiningOpPhase, related_name='ores',
+class MiningOpMineral(models.Model):
+    op = models.ForeignKey(MiningOp, related_name='minerals',
                               num_extra_on_change = 4,
                               min_num_in_admin = 6,
                               edit_inline = models.TABULAR)
-    type = models.ForeignKey(Item, limit_choices_to = {'group__category__name':'Asteroid', 'published':True},
+    type = models.ForeignKey(Item, limit_choices_to = {'group__name':'Mineral', 'published':True},
                              core=True)
     quantity = models.IntegerField()
     
     class Admin:
-        list_display = ['id', 'phase', 'type', 'quantity']
+        list_display = ['id', 'op', 'type', 'quantity']
         
     def __str__(self):
-        return "%s: %s x%d" % (self.phase, self.type, self.quantity)
+        return "%s: %s x%d" % (self.op, self.type, self.quantity)
