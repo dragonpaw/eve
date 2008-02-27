077c9f65cc16f82575f390b07bac5274fdf10c93
William Arnold <74992+dragonpaw@users.noreply.github.com>
2008-02-27 22:12:40 +0000

    Attempt at a smarter redistrubition method for POSes.

--- util/redistribute.py
+++ util/redistribute.py
@@ -11,8 +11,9 @@ from eve.user.models import Character
 from eve.pos.models import PlayerStation
 
 hour = 3600
-hour_from_now = datetime.utcnow() + timedelta(hours=1)
-six_from_now = datetime.utcnow() + timedelta(hours=6)
+now = datetime.utcnow()
+hour_from_now = now + timedelta(hours=1)
+six_from_now = now + timedelta(hours=6)
 
 def redistribute_characters():
     characters = Character.objects.all()
@@ -25,12 +26,13 @@ def redistribute_characters():
         print "%-30s | %s" % (c.name, c.cached_until)
 
 def redistribute_poses():
-    poses = PlayerStation.objects.all()
+    poses = PlayerStation.objects.all().order_by('cached_until')
     pos_span = timedelta(seconds=( hour*6/poses.count() ))
     x = timedelta(0)
     for p in poses:
         x += pos_span
-        p.cached_until = six_from_now + x
+        test = now + x
+        p.cached_until = max(test, p.cached_until) 
         p.save()
         print "%-30s | %s" % (p.name, p.cached_until)
         
