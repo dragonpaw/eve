f948f8a758b0d69023a9f73379588d80bef0a590
William Arnold <74992+dragonpaw@users.noreply.github.com>
2008-03-18 17:47:24 +0000

    Add POS template explaining situation with API. Redirect all POS urls to the above template. Catch the current brokeness with the API on director and POS refresh and handle it nicely.

--- pos/urls.py
+++ pos/urls.py
@@ -1,20 +1,23 @@
 from django.conf.urls.defaults import patterns
 
+#urlpatterns = patterns('eve.pos.views',
+#    (r'^$', 'pos_list'),
+#    
+#    (r'^(?P<station_id>\d+)/fuel/$', 'fuel_detail'),
+#    (r'^(?P<station_id>\d+)/profit/$', 'profit_detail'),
+#    (r'^(?P<station_id>\d+)/refuel/$', 'refuel'),
+#    (r'^(?P<station_id>\d+)/delegations/$', 'delegate_list'),
+#    (r'^(?P<station_id>\d+)/reactions/$', 'setup_reactions'),
+#    (r'^(?P<station_id>\d+)/grant/(?P<character_name>.+)/$', 'delegate_add'),
+#    (r'^(?P<station_id>\d+)/revoke/(?P<character_name>.+)/$', 'delegate_delete'),
+#
+#    (r'^profit/$', 'profit_list'),
+#    (r'^consumption/$', 'consumption'),
+#    
+#    (r'^helpers/$', 'monkey_list'),
+#    (r'^grant/(?P<grant>.+)/$', 'monkey_grant'),
+#    (r'^revoke/(?P<revoke>.+)/$', 'monkey_grant'),
+#)
 urlpatterns = patterns('',
-    (r'^$', 'eve.pos.views.pos_list'),
-    
-    (r'^(?P<station_id>\d+)/fuel/$', 'eve.pos.views.fuel_detail'),
-    (r'^(?P<station_id>\d+)/profit/$', 'eve.pos.views.profit_detail'),
-    (r'^(?P<station_id>\d+)/refuel/$', 'eve.pos.views.refuel'),
-    (r'^(?P<station_id>\d+)/delegations/$', 'eve.pos.views.delegate_list'),
-    (r'^(?P<station_id>\d+)/reactions/$', 'eve.pos.views.setup_reactions'),
-    (r'^(?P<station_id>\d+)/grant/(?P<character_name>.+)/$', 'eve.pos.views.delegate_add'),
-    (r'^(?P<station_id>\d+)/revoke/(?P<character_name>.+)/$', 'eve.pos.views.delegate_delete'),
-
-    (r'^profit/$', 'eve.pos.views.profit_list'),
-    (r'^consumption/$', 'eve.pos.views.consumption'),
-    
-    (r'^helpers/$', 'eve.pos.views.monkey_list'),
-    (r'^grant/(?P<grant>.+)/$', 'eve.pos.views.monkey_grant'),
-    (r'^revoke/(?P<revoke>.+)/$', 'eve.pos.views.monkey_grant'),
+    (r'.*', 'django.views.generic.simple.direct_to_template', {'template':'pos_api_broken.html'})
 )
