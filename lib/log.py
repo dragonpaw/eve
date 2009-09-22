460519fd1f161257d1381d8f5aa257706e916a24
William Arnold <74992+dragonpaw@users.noreply.github.com>
2009-09-22 12:08:35 -0700

    Ignore refresh errors when creating a new account and redirect to log no matter what. Re-write of alliance refresh job to check for more errors and use fewer variables.

--- lib/log.py
+++ lib/log.py
@@ -3,6 +3,7 @@ import logging
 from django_extensions.management.jobs import BaseJob as BaseJob_
 
 class BaseJob(BaseJob_):
+    log = None
 
     def logger(self):
         return logging.getLogger(self.__module__)
