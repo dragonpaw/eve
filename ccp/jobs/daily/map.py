8cf767c60ac69cc3b875ae03f4ee1542842ee696
William Arnold <74992+dragonpaw@users.noreply.github.com>
2009-09-22 12:13:06 -0700

    Move the 'meat' of the map and station jobs to their execute method.

--- ccp/jobs/daily/map.py
+++ ccp/jobs/daily/map.py
@@ -13,64 +13,61 @@ class Job(DailyJob):
     help = "Load map soverignity."
 
     def execute(self):
-        update_map()
+        print 'Starting galaxy map...'
+        api = eveapi.get_api()
 
-def update_map():
-    print 'Starting galaxy map...'
-    api = eveapi.get_api()
+        for s in api.map.Sovereignty().solarSystems:
+            system = SolarSystem.objects.get(pk=s.solarSystemID)
+            faction = None
+            if s.factionID != 0:
+                faction = Faction.objects.get(pk=s.factionID)
 
-    for s in api.map.Sovereignty().solarSystems:
-        system = SolarSystem.objects.get(pk=s.solarSystemID)
-        faction = None
-        if s.factionID != 0:
-            faction = Faction.objects.get(pk=s.factionID)
+            alliance = None
+            if s.allianceID != 0:
+                alliance = s.allianceID
 
-        alliance = None
-        if s.allianceID != 0:
-            alliance = s.allianceID
+            # Data cleanup.
+            if system.alliance_id == 0:
+                system.alliance = None
 
-        # Data cleanup.
-        if system.alliance_id == 0:
-            system.alliance = None
+            constellation_sov = None
+            if s.constellationSovereignty != 0:
+                constellation_sov = s.constellationSovereignty
 
-        constellation_sov = None
-        if s.constellationSovereignty != 0:
-            constellation_sov = s.constellationSovereignty
+            old = datetime.utcnow() - timedelta(days=7)
+            old = old.date()
 
-        old = datetime.utcnow() - timedelta(days=7)
-        old = old.date()
-
-        if system.alliance_id != alliance:
-            print("Differing: %s Old: %s, current: %s, new: %s" %
-                            (system.name, system.alliance_old_id,
-                             system.alliance_id, alliance))
-            if alliance != None and system.alliance == None:
-                # Don't overwrite old sovereignty data with null.
-                pass
-            else:
-                system.alliance_old = system.alliance
-            system.alliance_id = alliance
-            system.sov_time = datetime.utcnow()
-        elif system.sov_time == None:
-            # Set a default time of 2 weeks ago when we have no data.
-            # This only should occur when we reload the DB from CCP.
-            system.sov_time = datetime.utcnow() - timedelta(days=15)
-        elif system.sov_time < old and system.alliance_old != None:
-            # You only get a certain time to reclaim it.
-            system.alliance_old = None
+            if system.alliance_id != alliance:
+                print("Differing: %s Old: %s, current: %s, new: %s" %
+                                (system.name, system.alliance_old_id,
+                                 system.alliance_id, alliance))
+                if alliance != None and system.alliance == None:
+                    # Don't overwrite old sovereignty data with null.
+                    pass
+                else:
+                    system.alliance_old = system.alliance
+                system.alliance_id = alliance
+                system.sov_time = datetime.utcnow()
+            elif system.sov_time == None:
+                # Set a default time of 2 weeks ago when we have no data.
+                # This only should occur when we reload the DB from CCP.
+                system.sov_time = datetime.utcnow() - timedelta(days=15)
+            elif system.sov_time < old and system.alliance_old != None:
+                # You only get a certain time to reclaim it.
+                system.alliance_old = None
 
 
-        system.sov = s.sovereigntyLevel
-        system.faction = faction
+            system.sov = s.sovereigntyLevel
+            system.faction = faction
 
-        constellation = system.constellation
-        if constellation.alliance_id != constellation_sov:
-            if constellation.sov_time == None:
-                constellation.sov_time = datetime.utcnow() - timedelta(days=15)
-            else:
-                constellation.sov_time = datetime.utcnow()
-            constellation.alliance_id = constellation_sov
-            constellation.save()
+            constellation = system.constellation
+            if constellation.alliance_id != constellation_sov:
+                if constellation.sov_time == None:
+                    constellation.sov_time = datetime.utcnow() - timedelta(days=15)
+                else:
+                    constellation.sov_time = datetime.utcnow()
+                constellation.alliance_id = constellation_sov
+                constellation.save()
 
-        #print "Saving: %s" % system.name
-        system.save()
+            #print "Saving: %s" % system.name
+            system.save()
--- ccp/jobs/daily/stations.py
+++ ccp/jobs/daily/stations.py
@@ -1,36 +1,31 @@
 from django_extensions.management.jobs import DailyJob
-
 from eve.ccp.models import Station, Corporation, SolarSystem, Item
-
 from eve.lib import eveapi
 
 class Job(DailyJob):
     help = "Load conquerable stations."
 
     def execute(self):
-        update_stations()
+        print 'Starting outposts...'
+        api = eveapi.get_api()
 
-def update_stations():
-    print 'Starting outposts...'
-    api = eveapi.get_api()
-
-    for s in api.eve.ConquerableStationList().outposts:
-        corp_id = s.corporationID
-        corp_name = s.corporationName
-        try:
-            corporation = Corporation.objects.get(id=corp_id)
-        except Corporation.DoesNotExist:
-            corporation = Corporation(id=corp_id)
-            for m in corporation.refresh(name=corp_name):
-                print m
-        try:
-            station = Station.objects.get(id=s.stationID)
-        except Station.DoesNotExist:
-            print("Added station %s in system: %s" % (s.stationID, s.solarSystemID))
-            solarsystem = SolarSystem.objects.get(id=s.solarSystemID)
-            station = Station(id=s.stationID, solarsystem=solarsystem,
-                              region=solarsystem.region, constellation=solarsystem.constellation)
-        station.type = Item.objects.get(id=s.stationTypeID)
-        station.name = s.stationName
-        station.corporation = corporation
-        station.save()
+        for s in api.eve.ConquerableStationList().outposts:
+            corp_id = s.corporationID
+            corp_name = s.corporationName
+            try:
+                corporation = Corporation.objects.get(id=corp_id)
+            except Corporation.DoesNotExist:
+                corporation = Corporation(id=corp_id)
+                for m in corporation.refresh(name=corp_name):
+                    print m
+            try:
+                station = Station.objects.get(id=s.stationID)
+            except Station.DoesNotExist:
+                print("Added station %s in system: %s" % (s.stationID, s.solarSystemID))
+                solarsystem = SolarSystem.objects.get(id=s.solarSystemID)
+                station = Station(id=s.stationID, solarsystem=solarsystem,
+                                  region=solarsystem.region, constellation=solarsystem.constellation)
+            station.type = Item.objects.get(id=s.stationTypeID)
+            station.name = s.stationName
+            station.corporation = corporation
+            station.save()
